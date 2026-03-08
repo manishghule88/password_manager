51ba74f42c4326f25c43b973a74db66536b40ad9
manishghule88 <manishghule26@gmail.com>
2026-03-08 18:50:14 +0530

    dev: add decleration of menu methods

--- include/password_manager.h
+++ include/password_manager.h
@@ -4,10 +4,13 @@
 class PasswordManager {
 
 public:
+    PasswordManager();
+
     void start();
 
 private:
     std::string masterFileName = "master.dat";
+    std::string vaultFileName = "vault.dat";
     std::string storePassword = {};
     
     void initialize();
@@ -15,6 +18,12 @@ private:
     void createMasterFile();
     bool verifyMasterPassword();
     bool createMasterPassword();
+    void createMenu();
+    void addPassword();
+    void viewAllPassword();
+    void searchPassword();
+    void deletePassword();
+    void exitRun();
 };
 
 #endif
